c4b96fc6660340b7c9e21ec9a861b118409300a4
Santiago Lezica <slezica89@gmail.com>
2011-09-14 23:43:04 -0300

    Added save/send features to DisplayActivity

--- src/com/slezica/drexplorer/DisplayActivity.java
+++ src/com/slezica/drexplorer/DisplayActivity.java
@@ -16,10 +16,25 @@
 
 package com.slezica.drexplorer;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.slezica.drexplorer.tools.DrawableResource;
 
 public class DisplayActivity extends Activity {
 
@@ -28,18 +43,110 @@ public class DisplayActivity extends Activity {
 							RES_ID = "res_id";
 	}
 	
+	private TextView  mName;
+	private ImageView mIcon;
+	
+	private Button mSave, mSend;
+	
+	DrawableResource mResource;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.display);
 		
-		TextView  name = (TextView)  findViewById(R.id.name);
-		ImageView icon = (ImageView) findViewById(R.id.image);
+		mName = (TextView)  findViewById(R.id.name);
+		mIcon = (ImageView) findViewById(R.id.image);
+		
+		mSave = (Button) findViewById(R.id.save);
+		mSend = (Button) findViewById(R.id.send);
+		
+		mSave.setOnClickListener(mSaveListener);
+		mSend.setOnClickListener(mSendListener);
 		
 		Bundle extras = getIntent().getExtras();
 		
-		name.setText(extras.getString(EXTRAS.NAME));
-		icon.setImageResource(extras.getInt(EXTRAS.RES_ID));
+		mResource = new DrawableResource(
+	        extras.getInt(EXTRAS.RES_ID),
+	        extras.getString(EXTRAS.NAME)
+        );
+		
+		mName.setText(mResource.getName());
+		mIcon.setImageResource(mResource.getId());
 	}
 	
+	final OnClickListener mSaveListener = new OnClickListener() {
+        public void onClick(View view) {
+            try {
+                File file = save(mResource.getName(), generateBitmap());
+                toast("Saved drawable to:\n" + file.getPath());
+                
+            } catch (IOException e) {
+                toast("Failed to save: " + e.getMessage());
+            }
+        }
+	};
+	
+	final OnClickListener mSendListener = new OnClickListener() {
+        public void onClick(View view) {
+            try {
+                send(generateBitmap());
+                
+            } catch (IOException e) {
+                toast("Failed to send: " + e.getMessage());
+            }
+        }
+    };
+	
+	private void toast(String message) {
+	    Toast toast = Toast.makeText(
+                DisplayActivity.this,
+                message,
+                Toast.LENGTH_SHORT
+            );
+            
+        toast.setGravity(Gravity.BOTTOM, 0, mSend.getHeight());
+        toast.show();
+	}
+	
+	private Bitmap generateBitmap() {
+	    mIcon.buildDrawingCache();
+	    Bitmap bm = mIcon.getDrawingCache();
+	   
+	    return bm;
+	}
+	
+	private File save(String name, Bitmap bitmap) throws IOException {
+        File file = new File(getDirectory(), name + ".png");
+        FileOutputStream fOut = new FileOutputStream(file);
+        
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+        
+        fOut.flush();
+        fOut.close();
+        
+        return file;
+    }
+    
+    private void send(Bitmap bitmap) throws IOException {
+        String name = bitmap.toString() + "@" + System.currentTimeMillis();
+        
+        File file = save(name, bitmap);
+        
+        Intent intent = new Intent(Intent.ACTION_SEND);  
+        intent.setType("image/png"); 
+        intent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+        
+        startActivity(Intent.createChooser(intent, "Send this with..."));
+    }
+    
+    private File getDirectory() {
+        File dir = new File(Environment.getExternalStorageDirectory(), "drexplorer_saved");
+        
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+        
+        return dir;
+    }
 }
